33915bdee62ebb8a7ea1a3280551e52ed9bd02df
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-08-28 21:22:40 +0500

    header added

--- src/components/Blogsidebar/tests/history.test.tsx
+++ src/components/Blogsidebar/tests/history.test.tsx
@@ -2,35 +2,33 @@ import React from "react";
 import { render, RenderResult } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import HistoryContent from "../historyContent";
+import { shallow, ShallowWrapper } from "enzyme";
 
 describe("Launch History Item", () => {
-  let root: RenderResult;
+  let root: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
 
   beforeEach(() => {
-    root = render(
-      <Router>
-        <HistoryContent
-          title="mission title"
-          details="details"
-          articleLink="Article"
-          date="2020-08-27T19:19:00-04:00"
-        />
-      </Router>
+    root = shallow(
+      <HistoryContent
+        title="mission title"
+        details="details"
+        articleLink="Article"
+        date="2020-08-27T19:19:00-04:00"
+      />
     );
   });
 
   test("should render title", () => {
-    expect(root.getByText("mission title")).toBeTruthy();
-    console.log(root);
+    expect(root.contains("mission title")).toBeTruthy();
   });
   test("should render details", () => {
-    expect(root.getByText("details")).toBeTruthy();
+    expect(root.contains("details")).toBeTruthy();
   });
   test("should render Link", () => {
-    expect(root.getByText("Article")).toBeTruthy();
+    expect(root.contains("Article")).toBeTruthy();
   });
 
   test("should not render date", () => {
-    expect(root.queryByText("Augest 8th, 2020")).not.toBeTruthy();
+    expect(root.contains("Augest 8th, 2020")).not.toBeTruthy();
   });
 });
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div style={{ position: "absolute" }}>Space X</div>;
+  return (
+    <div className="w-full" style={{ position: "absolute" }}>
+      <header className="text-gray-500 bg-gray-900 body-font w-full">
+        <div className="customContainer mx-auto flex flex-wrap p-5 flex-row items-center">
+          <span className="flex title-font font-medium items-center text-white  mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">SpaceX</span>
+          </span>
+          <nav className="ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link to="/">
+              <span className="mr-5 hover:text-white cursor-pointer">Home</span>
+            </Link>
+            <Link to="/launches">
+              <span className="mr-5 hover:text-white cursor-pointer">
+                Launches
+              </span>
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
